c20d709339e566b2a62e73a7454825ed9272cff9
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-04-24 13:40:09 +1000

    added database
    database tables added using commands in nuget package manage console

--- TutoralRazorPages2/Services/ApplicationDBContext.cs
+++ TutoralRazorPages2/Services/ApplicationDBContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TutoralRazorPages2.Models;
 
 namespace TutoralRazorPages2.Services
 {
     public class ApplicationDBContext : DbContext
     {
         public ApplicationDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
