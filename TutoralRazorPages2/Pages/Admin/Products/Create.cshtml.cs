5a12e7934976135147fdf337c0f3e38d6b20bb7b
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-04-27 23:20:07 +1000

    added form validation

--- TutoralRazorPages2/Pages/Admin/Products/Create.cshtml.cs
+++ TutoralRazorPages2/Pages/Admin/Products/Create.cshtml.cs
@@ -1,15 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TutoralRazorPages2.Models;
+using TutoralRazorPages2.Services;
 
 namespace TutoralRazorPages2.Pages.Admin.Products
 {
     public class CreateModel : PageModel
     {
+        private readonly IWebHostEnvironment environment;
+        private readonly ApplicationDBContext context;
+
         [BindProperty]
         public ProductDto ProductDto { get; set; } = new ProductDto();
+
+        public CreateModel(IWebHostEnvironment environment, ApplicationDBContext context)
+        {
+            this.environment = environment;
+            this.context = context;
+        }
         public void OnGet()
         {
+        }
+
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public void OnPost() 
+        {
+
+            if (ProductDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ProductDto.ImageFile", "The image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                errorMessage = "Please provide all required fields";
+                return;
+            }
+
+            //save the image file
+
+
+            //save the new product in the database
+
+
+            //clear the form
+            ProductDto.Name = "";
+            ProductDto.Brand = "";
+            ProductDto.Category = "";
+            ProductDto.Price = 0;
+            ProductDto.Description = "";
+            ProductDto.ImageFile = null;
+
+            ModelState.Clear();
+
+            successMessage = "Product created successfully";
+
         }
     }
 }
